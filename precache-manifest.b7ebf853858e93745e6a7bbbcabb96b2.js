5a0a85aa91916260064bbf710604ded1911ef2f3
your <thegrimdarkstudios@gmail.com>
2020-09-16 01:19:02 +0200

    Updates

---
+++ precache-manifest.b7ebf853858e93745e6a7bbbcabb96b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eef9bd271bc5b6bfb6fcf732d2aec3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c13b25df19f38ad731b5",
+    "url": "/my-app/static/css/main.df2e6482.chunk.css"
+  },
+  {
+    "revision": "fee4ecd64a60d964a053",
+    "url": "/my-app/static/js/2.5de0fd1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.5de0fd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13b25df19f38ad731b5",
+    "url": "/my-app/static/js/main.3bcb7bf7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "87a67d48b166463853e07da549d7086e",
+    "url": "/my-app/static/media/Infierno.87a67d48.png"
+  },
+  {
+    "revision": "6ed8a14ed88b5355e6f8814e564ae51e",
+    "url": "/my-app/static/media/caja 0.6ed8a14e.png"
+  },
+  {
+    "revision": "75f201ff3405c1aa80511ca67280dff4",
+    "url": "/my-app/static/media/newlogo.75f201ff.png"
+  }
+]);
